c63bb1f106a5dc5ec57d6616f672f9fff301e6c5
Stélio Moiane <steliomo@gmail.com>
2015-11-24 08:47:59 +0200

    Criaçao do metodo para fecho do dialogo

--- src/main/java/mz/co/mozview/frameworks/jsf/util/DialogUtil.java
+++ src/main/java/mz/co/mozview/frameworks/jsf/util/DialogUtil.java
@@ -27,4 +27,8 @@ public class DialogUtil {
 
 		RequestContext.getCurrentInstance().openDialog(pageName, options, null);
 	}
+
+	public static void closeDialog(final Object object) {
+		RequestContext.getCurrentInstance().closeDialog(object);
+	}
 }
